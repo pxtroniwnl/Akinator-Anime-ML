a1b7dda70414aa328cfffd06abf4043989a6f452
pxtron <alejandropatronm@gmail.com>
2026-03-20 16:02:10 -0500

    changing lines in testing engine

--- play_test.py
+++ play_test.py
@@ -2,7 +2,7 @@ from src.engine import AkinatorEngine
 
 engine = AkinatorEngine('data/processed_characters.csv')
 
-print("🔮 ¡Bienvenido al Akinator de Anime! 🔮")
+print("¡Bienvenido al Akinator de Anime! 🔮")
 
 while len(engine.current_data) > 1:
     q = engine.get_best_question()
@@ -12,7 +12,7 @@ while len(engine.current_data) > 1:
     engine.update_data(q, val)
     
     if len(engine.current_data) == 0:
-        print("😱 No encontré a nadie con esas características...")
+        print("No encontré a nadie con esas características...")
         break
 
 result = engine.get_result()
